01c3ad94e262c93897b87bb71b0d730cff46c508
TechsWhatSheSaid <oliver20032@yahoo.com>
2026-01-19 10:14:22 -0500

    Add navigation toggle script to workflow images

---
+++ .github/workflows/images/script.js
@@ -0,0 +1,8 @@
+<script>
+    const navToggle = document.querySelector('.nav-toggle');
+    const nav = document.querySelector('nav');
+
+    navToggle.addEventListener('click', () => {
+        nav.classList.toggle('show');
+    });
+</script>
